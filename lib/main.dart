46badfec7c6f69d0057aefea1f4d0a02396f5e7a
Mac <mac@Macs-MacBook-Pro.local>
2022-01-11 19:52:19 +0530

    update pubspec.yml file

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,41 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void onPressAnswerQuestionButton() {
+    print('Pressed 1 answer!');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite movie?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Basics'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[1],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: onPressAnswerQuestionButton,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Pressed 2 answer!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print('Pressed 3 answer!'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
